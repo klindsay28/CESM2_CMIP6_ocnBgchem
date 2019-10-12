28e05cb658e5b25d08ad8ab97529b7cadac2dd16
Keith Lindsay <klindsay@ucar.edu>
2019-10-12 16:40:18 -0600

    add notebook showing how to compute and save monthly climatology

---
+++ utils.py
@@ -0,0 +1,83 @@
+"""utility functions"""
+
+import cftime
+import numpy as np
+
+def copy_fill_settings_ds(ds_in, ds_out):
+    """
+    propagate _FillValue and missing_value settings from vars in ds_in to ds_out
+    return ds_out
+    """
+    for varname in ds_in.variables:
+        if varname in ds_out.variables:
+            copy_fill_settings_da(ds_in[varname], ds_out[varname])
+    return ds_out
+
+def copy_fill_settings_da(da_in, da_out):
+    """
+    propagate _FillValue and missing_value settings from da_in to da_out
+    return da_out
+    """
+    if '_FillValue' in da_in.encoding:
+        da_out.encoding['_FillValue'] = da_in.encoding['_FillValue']
+    else:
+        da_out.encoding['_FillValue'] = None
+    return da_out
+
+def dim_cnt_check(ds, varname, dim_cnt):
+    """confirm that varname in ds has dim_cnt dimensions"""
+    if len(ds[varname].dims) != dim_cnt:
+        msg_full = 'unexpected dim_cnt=%d, varname=%s' % (len(ds[varname].dims), varname)
+        raise ValueError(msg_full)
+
+def time_set_mid(ds, time_name):
+    """
+    set ds[time_name] to midpoint of ds[time_name].attrs['bounds'], if bounds attribute exists
+    type of ds[time_name] is not changed
+    ds is returned
+    """
+
+    if 'bounds' not in ds[time_name].attrs:
+        return ds
+
+    # determine units and calendar of unencoded time values
+    if ds[time_name].dtype == np.dtype('O'):
+        units = 'days since 0000-01-01'
+        calendar = 'noleap'
+    else:
+        units = ds[time_name].attrs['units']
+        calendar = ds[time_name].attrs['calendar']
+
+    # construct unencoded midpoint values, assumes bounds dim is 2nd
+    tb_name = ds[time_name].attrs['bounds']
+    if ds[tb_name].dtype == np.dtype('O'):
+        tb_vals = cftime.date2num(ds[tb_name].values, units=units, calendar=calendar)
+    else:
+        tb_vals = ds[tb_name].values
+    tb_mid = tb_vals.mean(axis=1)
+
+    # set ds[time_name] to tb_mid
+    if ds[time_name].dtype == np.dtype('O'):
+        ds[time_name].values = cftime.num2date(tb_mid, units=units, calendar=calendar)
+    else:
+        ds[time_name].values = tb_mid
+
+    return ds
+
+def time_year_plus_frac(ds, time_name):
+    """return time variable, as year plus fraction of year"""
+
+    # this is straightforward if time has units='days since 0000-01-01' and calendar='noleap'
+    # so convert specification of time to that representation
+
+    # get time values as an np.ndarray of cftime objects
+    if np.dtype(ds[time_name]) == np.dtype('O'):
+        tvals_cftime = ds[time_name].values
+    else:
+        tvals_cftime = cftime.num2date(
+            ds[time_name].values, ds[time_name].attrs['units'], ds[time_name].attrs['calendar'])
+
+    # convert cftime objects to representation mentioned above
+    tvals_days = cftime.date2num(tvals_cftime, 'days since 0000-01-01', calendar='noleap')
+
+    return tvals_days / 365.0
